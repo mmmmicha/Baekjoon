54b23280f416e3d133494cde5f4ab44cf1f9293d
wjdrhkd456 <56371387+wjdrhkd456@users.noreply.github.com>
2020-04-12 16:47:56 +0900

    각행,열은 반복변수의 배수라는 것을 이용해야함.

--- src/binarysearch/KthNumber.java
+++ src/binarysearch/KthNumber.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
  * 
  * 2020-04-08 좀 더 고민해보기 (미완성)
  * 
+ * 2020-04-12 mid 를 k번째수에 해당하는 숫자로 기준하고 문제를 풀기(해결)
+ * 
  * */
 
 public class KthNumber
@@ -20,7 +22,32 @@ public class KthNumber
 		int n = sc.nextInt();
 		int k = sc.nextInt();
 		
-		int min = 1;
+		int min = 1, max = k, mid = 0, answer = 0, temp = 0;
+		
+		while(min<=max)
+		{
+			mid = (max+min)/2;
+			
+			temp=0;
+			
+			for(int i=1; i<=n; i++)
+			{
+				temp += Math.min(mid/i, n);
+			}
+			
+			if(temp>=k)
+			{
+				answer = mid;
+				max = mid-1;
+			}
+			else
+			{
+				min = mid+1;
+			}
+			
+		}
+		
+		System.out.println(answer);
 		
 	}
 }
